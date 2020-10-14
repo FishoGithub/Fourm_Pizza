1f1bddfede03d0989182beb9a8e8f6192a0c6704
MihirMagikarp <mihir.majgaonkar@gmail.com>
2020-10-14 00:13:47 +0000

    all the js and worked on form validation

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-let toppers = document.getElementsByName('toppings-options')
-
-function onSubmit() {
-  let zipcode = document.getElementById('zip')
-  console.log(zipcode.value);
-
-  if(zipcode === '98052') {
-    console.log('correct');
-  }
-  else if (zipcode === !'98052') {
-    console.log('no')
-  }
+function onSubmit(){
+  var zip =document.getElementById('zip').value
+  var zipinput = document.getElementById('zip')
+  
+  if(zip == "98052"){
+  zipinput.style.background = "green";
 }
+else{
+  zipinput.style.background = "red";
+}
+}
+
+let toppers = document.getElementsByName('toppings-options')
+toppers.forEach(node => console.log(node.checked))
 
 function checkBoxes() {
-  for(var i = 0; i<toppers.length; i++) {
-    if(toppers[i].checked) {
-      console.log(toppers[i].value);
-    }
+  for (let i = 0; i<toppers.length; i++) {
+    array = []
+    array.push(toppers[i])
+  }
+  if (array.length > 3) {
+    alert('TOO MANY TOPPINGS');
   }
 }
 
+function validation() {
+  let pizzasize = document.getElementById('size-input');
+  let crusttype = document.getElementById('pizza-crust');
+  var zip = document.getElementById('zip');
+  answers = {
+    size: pizzasize.value,
+    crust: crusttype.value,
+    zip_code: zip.value
+
+  }
+  console.log(answers);
+}
+
+//boilerplate, make sure to change later to show values of all inputs
+function submitted() {
+  validation();
+}
